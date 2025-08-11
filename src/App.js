abac36e5161aadbce57917dc0b35950474d216c4
Hoang Cao Nhat Minh <hoangcaonhatminh@gmail.com>
2025-08-11 11:41:41 +0700

    Create project 'slot1-ex1'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">          
+          <h1>
+              Hello React!
+          </h1>
+
+          {/* Đây là comment trong JSX <p>
+             My name is, {myName}
+          </p> */}
+
+
+          <p>
+             My name is Minh!
+          </p>
+        
       </header>
     </div>
   );
